acc243b55961436e49114230cd60b9cdc504c0f6
Edu <107009101+educoppi@users.noreply.github.com>
2023-06-14 17:26:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula08repeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomes = new List<string>();
+
+            nomes.Add("Stella");
+            nomes.Add("William");
+            nomes.Add("Luis");
+            nomes.Add("Luis");
+            nomes.Add("Luis");
+            nomes.Add("Luis");
+            nomes.Add("Luis");
+            nomes.Add("Luis");
+            nomes.Add("Luis");
+            nomes.Add("Luis");
+
+            int contador = 0;
+            while (contador < nomes.Count)
+            {
+                Console.WriteLine("O nome é " + nomes[contador]);
+                contador++;
+            }
+            Console.ReadKey();
+        }
+        void repeticao()
+        {
+            //Repetições (loop)
+
+            int contador = 0;
+
+            while (contador < 5)
+            {
+                Console.WriteLine("Olá");
+                //formas  de realizar repetição
+                contador = contador + 1;
+                contador += 1;
+                contador++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
